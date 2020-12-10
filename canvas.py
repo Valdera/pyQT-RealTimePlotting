1fb6543e60f10c5f696c522bd2e24d84237a3f75
Fauzan Valdera <f.valdera@yahoo.co.id>
2020-12-10 17:35:28 +0700

    update canvas

--- canvas.py
+++ canvas.py
@@ -18,4 +18,28 @@ class MplCanvas(FigureCanvas):
     def updateRange(self,min_x,max_x):
         self.axes.set_xlim(xmin= min_x, xmax = max_x)
         self.axes.set_ylim(ymin= 0, ymax = 15)
-    
+
+def initCanvas(self):
+    self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
+    toolbar = NavigationToolbar(self.canvas, self)
+    self._plot_ref = None
+
+
+     # Update Matplotlib Graph
+# def update_plot(self, new_y):
+#     global flagTime, delayTime
+#     if(flagTime > 1/delayTime):
+#         flagTime = 0
+#         self.seconds.append(self.seconds[-1]+1)
+#     else:
+#         flagTime += 1
+#     self.y.append(new_y)
+#     # self.x.append(self.x[-1] + delayTime)
+#     self.canvas.axes.cla()  # Clear the canvas.
+#     self.canvas.axes.plot(self.x, self.y, 'r')
+#     if(len(self.seconds) >= 15):
+#         self.x = self.x[1:]
+#         self.y = self.y[1:]
+#         self.xRange += delayTime
+#     self.canvas.updateRange(self.xRange, self.xRange + 20)
+#     self.canvas.draw()
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtGui import *
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys
+from serial import Serial
+import serial
 import platform
 import time
 from random import randint
@@ -22,7 +24,41 @@ flagTime = 0
 flagThread = True
 delayTime = 0.05
 
+def serial_ports():
+    '''
+    Get all the active port in the computer
+    '''
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+def connect_port(port):
+    '''
+    Connect to the port
+    '''
+    serialPort = serial.Serial(port = port, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+    return serialPort
+
 def initButton(self):
+    '''
+    Init all the button required
+    '''
     self.Btn_Toggle.clicked.connect(lambda: UIFunctions.toggleMenu(self, 250, True))
     self.btn_page_1.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_1))
     self.btn_page_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
@@ -31,24 +67,28 @@ def initButton(self):
     self.stopButton.pressed.connect(lambda: self.stopThreading())
 
 def initGraph(self):
+    '''
+    Init the main graph
+    '''
+    # Plot the graph
     self.graphWidget = pg.PlotWidget()
+    # GUI Configuration
     self.graphWidget.setBackground('w')
     self.graphWidget.setTitle("Your Title Here", color="b", size="30pt")
     styles = {"color": "#f00", "font-size": "20px"}
     self.graphWidget.setLabel("left", "Temperature (°C)", **styles)
     self.graphWidget.setLabel("bottom", "Hour (H)", **styles)
     pen = pg.mkPen(color='r', width=2)
+    # Set the data and the range
     self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
     self.graphWidget.setXRange(self.xRange, self.xRange+50, padding=0)
     self.graphWidget.showGrid(x=True, y=True)
 
-def initCanvas(self):
-    self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
-    toolbar = NavigationToolbar(self.canvas, self)
-    self._plot_ref = None
-
 class MainWindow(QMainWindow,Ui_MainWindow):
     def __init__(self, *args, obj=None,**kwargs):
+        '''
+        Init class
+        '''
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.threadpool = QThreadPool()
@@ -68,48 +108,57 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         # graphLayout.addWidget(toolbar)
         graphLayout.addWidget(self.graphWidget)
 
-
-        # Graph GUI
-        # initCanvas(self)
-
-
         # Insert Layout
         self.frameGraph.setLayout(graphLayout)
 
-    # Stop Threading Button
     def stopThreading(self):
+        '''
+        The function to stop the thread
+        '''
         Worker.flagThread = False
 
-    # Threading Function
     def execute_this_fn(self):
+        '''
+        The function to be executed when the thread runs
+        '''
         global delayTime
         y = randint(0, 10)
         time.sleep(delayTime)
         return y 
 
-    # Threading Complete
     def thread_complete(self):
+        '''
+        This function run after thread finished
+        '''
         Worker.flagThread = True
         print("THREAD COMPLETE")
     
-    # Threading Process
     def result_process(self, new_y):
+        '''
+        Getting the result from the output
+        '''
         self.update_plot_graph(new_y)
 
-    # Threading Maker
     def execute_thread(self):
+        '''
+        Execute the thread
+        '''
         worker = Worker(self.execute_this_fn) 
         worker.signals.result.connect(self.result_process)
         worker.signals.finished.connect(self.thread_complete)
         self.threadpool.start(worker)
 
-    # Close all Thread before quit
     def closeEvent(self, event):
+        '''
+        Close all thread before quit
+        '''
         Worker.flagThread = False
         self.threadpool.clear()
 
-    # Update PyQT Graph
     def update_plot_graph(self, new_y):
+        '''
+        Update Graph Data
+        '''
         global flagTime, delayTime
         if(flagTime > 1/delayTime):
             flagTime = 0
@@ -126,47 +175,10 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.x.append(self.x[-1] + delayTime)  # Add a new value 1 higher than the last.
         self.y.append(new_y)  # Add a new random value.
         self.data_line.setData(self.x, self.y)  # Update the data.
-
-    # Update Matplotlib Graph
-    def update_plot(self, new_y):
-        global flagTime, delayTime
-        if(flagTime > 1/delayTime):
-            flagTime = 0
-            self.seconds.append(self.seconds[-1]+1)
-        else:
-            flagTime += 1
-        self.y.append(new_y)
-        # self.x.append(self.x[-1] + delayTime)
-        self.canvas.axes.cla()  # Clear the canvas.
-        self.canvas.axes.plot(self.x, self.y, 'r')
-        if(len(self.seconds) >= 15):
-            self.x = self.x[1:]
-            self.y = self.y[1:]
-            self.xRange += delayTime
-        self.canvas.updateRange(self.xRange, self.xRange + 20)
-        self.canvas.draw()
-
-    def update_plot_data(self, new_y):
-        global flagTime, delayTime
-        if(flagTime > 1/delayTime):
-            flagTime = 0
-            self.seconds.append(self.seconds[-1]+1)
-        else:
-            flagTime += 1
-        
-        if(len(self.seconds) > 30):
-            self.xRange += delayTime
-            print(self.xRange)
-            # self.graphWidget.setYRange(30, 40, padding=0)
-            self.graphWidget.setXRange(self.xRange, self.xRange+50, padding=0)
-
-        self.x.append(self.x[-1] + delayTime)  # Add a new value 1 higher than the last.
-        self.y.append(new_y)  # Add a new random value.
-        self.data_line.setData(self.x, self.y)  # Update the data.
-        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    print(serial_ports())
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
